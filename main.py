0dfe7e66fee3b436bd0c7384754501088f85b905
christianrcorona <christian.corona@students.fhu.edu>
2024-02-10 15:48:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 #Remove pass and complete the code
 def check_character(word, index):
-   pass
-
-if __name__ == '__main__': 
+    if 0 <= index < len(word): #just to make sure we aren't getting weird numbers bro
+        char = word[index]
+        if char.isalpha():
+            return "letter"
+        elif char.isdigit():
+            return "digit"
+        elif char.isspace():
+            return "white space"
+        else:
+            return "unkown"
+    else:
+        return "Get the index right"        
+if __name__ == "__main__":
     print(check_character('happy birthday', 2))
     print(check_character('happy birthday', 5))
     print(check_character('happy birthday 2 you', 15))
